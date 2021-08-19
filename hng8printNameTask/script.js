ed28505ad7bb27cb9ac14522e3fcb49cba55baaa
Ekaji Onah <45511914+Ekaji@users.noreply.github.com>
2021-08-19 23:36:12 +0100

    Update script.js

--- hng8printNameTask/script.js
+++ hng8printNameTask/script.js
@@ -1,1 +1,32 @@
 
+let myText = document.getElementsByClassName("demo")[0]
+let text = " Ekaji Onah.       ";       
+let i = 0;
+let speed = 150;
+
+const cssStyles = [
+    { style: 'Josefin_Sans', background: '#EE666B' }, 
+    { style: 'Lora', background: '#F24F50' },
+    { style: 'Dosis', background: '#101B1F' },
+]
+
+
+
+
+let typewrite = () => {
+
+    
+    if(i < text.length){
+        myText.className = 'Josefin_Sans'
+        myText.textContent += text.charAt(i) ;
+        i++;
+        if(i >= text.length){
+            i = 0;
+            myText.textContent = text.charAt(i);
+            i++
+            
+        }
+setTimeout( typewrite, speed );	 
+}
+}
+typewrite()
